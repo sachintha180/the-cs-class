5c0f11009bef6769d7c8f0721234ba89df6bcc1e
sachintha180 <sachinthasenanayake180@gmail.com>
2025-06-26 13:58:42 +0530

    update build

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/the-cs-class">
       <div className="min-h-screen bg-gray-900 font-body">
         <Routes>
           <Route path="/" element={<LandingPage />} />
